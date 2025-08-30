0500e830bea4bafb7c73c37e53c3b0b4672373bd
phiphipiupiu <128450965+phiyixian@users.noreply.github.com>
2025-08-30 16:12:36 +0800

    Colorful arcs and lines

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -28,35 +28,51 @@ const sketch = () => {
     const h = height * 0.1;
     let x, y;
 
-    const num = 40;
+    const num = 100;
     const radius = width * 0.3;
 
     for (let i = 0; i < num; i++) {
+      context.strokeStyle = `hsl(${160 + 3*i}, 100%, 50%)`;
       const slice = math.degToRad(360/num);
       const angle = slice * i;
 
-      x = cx + radius * Math.sin(angle);
-      y = cy + radius * Math.cos(angle);
+      x = radius * Math.sin(angle);
+      y = radius * Math.cos(angle);
 
+      // Drawing verticle rectangles
       context.save();
+      context.fillStyle = 'black';
       context.translate(x, y);
       context.rotate(-angle);
-      context.scale(random.range(0.1, 2), random.range(0.2, 0.5));
+      context.scale(random.range(0.5, 4), random.range(1, 3));
 
       context.beginPath();
-      context.rect(-w / 2, random.range(0, -h * 0.5), w, h);
+      context.rect(w, random.range(0, h * 5), w, h);
       context.fill();
       context.restore();
 
       // Drawing arc
       context.save();
-      context.translate(cx, cy);
+      // context.translate(cx, cy);
+      context.rotate(angle);
+
+      context.lineWidth = random.range(15, 25);
+
+      context.beginPath();
+      context.arc(0, 0, random.range(0.5, 3), slice * random.range(0, -2), slice * random.range(5, 10));
+      context.stroke()
+
+      context.restore();
+
+      // Drawing smaller arc
+      context.save();
+      // context.translate(cx, cy);
       context.rotate(angle);
 
       context.lineWidth = random.range(5, 20);
 
       context.beginPath();
-      context.arc(0, 0, radius * random.range(0.7, 1.3), slice * random.range(0, -8), slice * random.range(1, 5));
+      context.arc(0, 0, radius * random.range(2, 3.5), slice * random.range(0, -5), slice * random.range(1, 5));
       context.stroke()
 
       context.restore();
